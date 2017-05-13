a942ead8a5d4a23c3be340ac9fb244d7981ef2c4
v0idwalker <pales.michael@gmail.com>
2017-05-13 18:51:11 +0200

    NER/SA

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,9 @@
 # fetch data
-# for theGuardian: response.xpath('//a[contains(@data-link-name, "article")]').extract() response.css('li.l-row__item').extract()  E
-# for Independent: response.css('.content h1 a')
-import scrapy
-from crawl.crawl import spiders
+import requests
+from bs4 import BeautifulSoup
 
-scrapy
+def content_body():
+    r = requests.get("url")
+    bsc = BeautifulSoup(r.content)
+    bsc.find("")
+    # remove HTML tags, get only the text.
